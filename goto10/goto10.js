14c1f690bf674d677b0356ff7c161e1f15a132a6
mgiacopu <marco.giaco2000@gmail.com>
2018-01-30 14:58:25 +0100

    update

---
+++ goto10/goto10.js
@@ -0,0 +1,28 @@
+let scaling, px, py;
+
+function setup() {
+    createCanvas(600, 600);
+    background(0);
+    scaling = 30;
+    px = 0;
+    py = 0;
+}
+
+function draw() {
+    translate(px*scaling, py*scaling);
+    stroke(255);
+    if (random() > 0.5) {
+        line(0, 0, scaling, scaling); 
+        px++;
+    } else {
+        line(scaling, 0, 0, scaling);
+        px++;
+    }
+    if (px * scaling > width) {
+        px = 0;
+        py++;
+    }
+    if (py * scaling > height) {
+        noLoop();
+    }
+}
