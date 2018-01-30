cc681e0c83fb05b8b61be6f0a23342aacdd4f828
mgiacopu <marco.giaco2000@gmail.com>
2018-01-30 14:42:47 +0100

    orbite and goto10

---
+++ orbite/orbite.js
@@ -0,0 +1,21 @@
+let terra;
+let venere;
+let angle;
+
+function setup() {
+    createCanvas(600, 600);
+    terra = new p5.Vector(0, -200);
+    venere = new p5.Vector(0, -100);
+    angle = PI / 60;
+    frameRate(30);
+	background(0);
+}
+
+function draw() {
+    translate(300, 300);
+    stroke(255, 255, 255, 100);
+	terra.rotate(angle / 13 * 8);
+	venere.rotate(angle);
+	strokeWeight(1);
+	line(venere.x, venere.y, terra.x, terra.y);
+}
